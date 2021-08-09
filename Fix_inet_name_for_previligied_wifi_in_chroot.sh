9c5e11c71e9fd51fc0ed943748d598e3a94819a2
Mirivan <54078891+Mirivan@users.noreply.github.com>
2021-08-09 15:02:04 +0500

    Update Fix_inet_name_for_previligied_wifi_in_chroot.sh

--- Fix_inet_name_for_previligied_wifi_in_chroot.sh
+++ Fix_inet_name_for_previligied_wifi_in_chroot.sh
@@ -1,2 +1,36 @@
 #! /bin/sh
-echo $(pwd) > /previous.TO; cd /etc; if [ -f "/etc/group" ]; then typo=`grep -w "android_inet\|aid_inet\|inet" group`; if [ -z $typo ]; then echo "ANDROID inet group doesn't exists!"; else sed -i s/$typo/"inet:x:3003:"/g group; echo "Setup done! Now u must add root user to new inet group with: usermod -a -G inet root"; fi; else echo "/etc/group file doesn't exist"; fi; cd $(cat /previous.TO); rm /previous.TO;
+######### Checking paranoid network #########
+MNT="/data/local/nhsystem/kali-arm64"
+echo "[!] Checking paranoid network..."
+if [ -f "/proc/config.gz" ]; then
+    config="CONFIG_ANDROID_PARANOID_NETWORK"
+    get_result=`zcat /proc/config.gz | grep $config`
+    if [ -z get_result ]; then
+        echo "[!] Paranoid network not detected in kernel, skipping..."
+    else
+        echo "[+] Paranoid network detected, setting up inet..."
+        if [ -f "$MNT/etc/group" ]; then
+            check_inet=`egrep -w "android_inet|aid_inet" $MNT/etc/group`
+            check_group=`grep -w "3003" $MNT/etc/group`
+            grep_gi=`echo $check_inet | grep "3003"`
+            if [ -z $check_inet ]; then
+                echo "[!] Android inet group not found! Adding..."
+                if [ ! -z $check_group ]; then
+                    # Group 3003 is found
+                    if [ -z $grep_gi ]; then
+                        echo "[-] Group 3003 is busy and it isn't at Android inet. Skipping..."
+                    else
+                        echo "inet:x:3003:root" | tee -a $MNT/etc/group
+                    fi
+                fi
+            else
+                sed -i s/"$(echo $check_inet | cut -d ":" -f 1)"/"inet"/g $MNT/etc/group
+                echo "[+] Adding inet (3003) group done."
+            fi
+        else
+            echo "/etc/group file doesn't exist"
+        fi
+    fi
+else
+    echo "[!] Config file not found, skipping..."
+fi
